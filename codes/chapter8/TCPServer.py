1201a0605d9c61c032c12b515add0e4baf19acba
bigen1925 <bigen1925@gmail.com>
2020-10-29 23:17:24 +0900

    add chapter8

---
+++ codes/chapter8/TCPServer.py
@@ -0,0 +1,52 @@
+import socket
+
+
+class TCPServer:
+    """
+    TCP通信を行うサーバーを表すクラス
+    """
+    def serve(self):
+        """
+        サーバーを起動する
+        """
+
+        print("=== サーバーを起動します ===")
+
+        try:
+            # socketを生成
+            server_socket = socket.socket()
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # socketをlocalhostのポート8080番に割り当てる
+            server_socket.bind(("localhost", 8080))
+            server_socket.listen(10)
+
+            # 外部からの接続を待ち、接続があったらコネクションを確立する
+            print("=== クライアントからの接続を待ちます ===")
+            (client_socket, address) = server_socket.accept()
+            print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
+
+            # クライアントから送られてきたデータを取得する
+            request = client_socket.recv(4096)
+
+            # クライアントから送られてきたデータをファイルに書き出す
+            with open("server_recv.txt", "wb") as f:
+                f.write(request)
+
+            # クライアントへ送信するレスポンスデータをファイルから取得する
+            with open("server_send.txt", "rb") as f:
+                response = f.read()
+
+            # クライアントへレスポンスを送信する
+            client_socket.send(response)
+
+            # 通信を終了させる
+            client_socket.close()
+
+        finally:
+            print("=== サーバーを停止します。 ===")
+
+
+if __name__ == '__main__':
+    server = TCPServer()
+    server.serve()
