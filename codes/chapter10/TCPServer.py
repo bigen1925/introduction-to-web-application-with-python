279a435b7571f650dbbf39b4934792e990441756
bigen1925 <bigen1925@gmail.com>
2020-11-09 19:47:24 +0900

    add a sorce code of chapter10

---
+++ codes/chapter10/TCPServer.py
@@ -0,0 +1,63 @@
+import socket
+from datetime import datetime
+
+
+class TCPServer:
+    """
+    TCP通信を行うサーバーを表すクラス
+    """
+    def serve(self):
+        """
+        サーバーを起動する
+        """
+
+        print("=== サーバーを起動します ===")
+
+        try:
+            # socketを生成
+            server_socket = socket.socket()
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # socketをlocalhostのポート8080番に割り当てる
+            server_socket.bind(("localhost", 8080))
+            server_socket.listen(10)
+
+            # 外部からの接続を待ち、接続があったらコネクションを確立する
+            print("=== クライアントからの接続を待ちます ===")
+            (client_socket, address) = server_socket.accept()
+            print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
+
+            # クライアントから送られてきたデータを取得する
+            request = client_socket.recv(4096)
+
+            # クライアントから送られてきたデータをファイルに書き出す
+            with open("server_recv.txt", "wb") as f:
+                f.write(request)
+
+            # レスポンスボディを生成
+            response_body = "<html><body><h1>It works!</h1></body></html>"
+
+            # レスポンスヘッダーを生成
+            response_header = ""
+            response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+            response_header += "Host: HenaServer/0.1\r\n"
+            response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+            response_header += "Connection: Close\r\n"
+            response_header += "Content-Type: text/html\r\n"
+
+            # ヘッダーとボディを空行でくっつけた上でbytesに変換し、レスポンス全体を生成する
+            response = (response_header + "\r\n" + response_body).encode()
+
+            # クライアントへレスポンスを送信する
+            client_socket.send(response)
+
+            # 通信を終了させる
+            client_socket.close()
+
+        finally:
+            print("=== サーバーを停止します。 ===")
+
+
+if __name__ == '__main__':
+    server = TCPServer()
+    server.serve()
