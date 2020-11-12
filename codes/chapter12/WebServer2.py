e3c089111aa5d1ffdb4d425fe431b810943e987a
bigen1925 <bigen1925@gmail.com>
2020-11-12 16:21:50 +0900

    add a source code of chapter12

---
+++ codes/chapter12/WebServer2.py
@@ -0,0 +1,128 @@
+import os
+import socket
+import traceback
+from datetime import datetime
+
+
+class WebServer:
+    """
+    Webサーバーを表すクラス
+    """
+    # 実行ファイルのあるディレクトリ
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # 静的配信するファイルを置くディレクトリ
+    DOCUMENT_ROOT = os.path.join(BASE_DIR, "static")
+
+    # 拡張子とMIME Typeの対応
+    MIME_TYPES = {
+        "html": "text/html",
+        "htm": "text/html",
+        "txt": "text/plain",
+        "css": "text/css",
+        "js": "application/javascript",
+        "png": "image/png",
+        "jpg": "image/jpg",
+        "jpeg": "image/jpg",
+        "gif": "image/gif",
+    }
+
+    def serve(self):
+        """
+        サーバーを起動する
+        """
+
+        print("=== サーバーを起動します ===")
+
+        try:
+            # socketを生成
+            server_socket = socket.socket()
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+            # socketをlocalhostのポート8080番に割り当てる
+            server_socket.bind(("localhost", 8080))
+            server_socket.listen(10)
+
+            while True:
+                # 外部からの接続を待ち、接続があったらコネクションを確立する
+                print("=== クライアントからの接続を待ちます ===")
+                (client_socket, address) = server_socket.accept()
+                print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
+
+                try:
+                    # クライアントから送られてきたデータを取得する
+                    request = client_socket.recv(4096)
+
+                    # クライアントから送られてきたデータをファイルに書き出す
+                    with open("server_recv.txt", "wb") as f:
+                        f.write(request)
+
+                    # リクエスト全体を
+                    # 1. リクエストライン(1行目)
+                    # 2. リクエストボディ(2行目〜空行)
+                    # 3. リクエストボディ(空行〜)
+                    # にパースする
+                    request_line, remain = request.split(b"\r\n", maxsplit=1)
+                    request_headers, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
+
+                    # リクエストラインをパースする
+                    method, path, http_version = request_line.decode().split(" ")
+
+                    # pathの先頭の/を削除し、相対パスにしておく
+                    relative_path = path.lstrip("/")
+                    # ファイルのpathを取得
+                    static_file_path = os.path.join(self.DOCUMENT_ROOT, relative_path)
+
+                    # ファイルからレスポンスボディを生成
+                    try:
+                        with open(static_file_path, "rb") as f:
+                            response_body = f.read()
+
+                        # レスポンスラインを生成
+                        response_line = "HTTP/1.1 200 OK\r\n"
+
+                    except OSError:
+                        # ファイルが見つからなかった場合は404を返す
+                        response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                        response_line = "HTTP/1.1 404 Not Found\r\n"
+
+                    # ヘッダー生成のためにContent-Typeを取得しておく
+                    # pathから拡張子を取得
+                    if "." in path:
+                        ext = path.rsplit(".", maxsplit=1)[-1]
+                    else:
+                        ext = ""
+                    # 拡張子からMIME Typeを取得
+                    # 知らない対応していない拡張子の場合はoctet-streamとする
+                    content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+
+                    # レスポンスヘッダーを生成
+                    response_header = ""
+                    response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+                    response_header += "Host: HenaServer/0.1\r\n"
+                    response_header += f"Content-Length: {len(response_body)}\r\n"
+                    response_header += "Connection: Close\r\n"
+                    response_header += f"Content-Type: {content_type}\r\n"
+
+                    # レスポンス全体を生成する
+                    response = (response_line + response_header + "\r\n").encode() + response_body
+
+                    # クライアントへレスポンスを送信する
+                    client_socket.send(response)
+
+                except Exception:
+                    # リクエストの処理中に例外が発生した場合はコンソールにエラーログを出力し、
+                    # 処理を続行する
+                    print("リクエストの処理中にエラーが発生しました。")
+                    traceback.print_exc()
+
+                finally:
+                    # 例外が発生した場合も、発生しなかった場合も、TCP通信のcloseは行う
+                    client_socket.close()
+
+        finally:
+            print("=== サーバーを停止します。 ===")
+
+
+if __name__ == '__main__':
+    server = WebServer()
+    server.serve()
