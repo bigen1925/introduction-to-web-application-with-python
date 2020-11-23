ca431cd950d7578621124d0b5afaead4b969c856
bigen1925 <bigen1925@gmail.com>
2020-11-23 18:21:17 +0900

    codes chapter16

---
+++ codes/chapter16-3/http/response.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+
+class HTTPResponse:
+    status_code: int
+    content_type: Optional[str]
+    body: bytes
+
+    def __init__(self, status_code: int = 200, content_type: str = None, body: bytes = b""):
+        self.status_code = status_code
+        self.content_type = content_type
+        self.body = body
